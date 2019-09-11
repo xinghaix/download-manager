dc0cbec4c428e879f5215dce4c7f3dbc68820944
luxin <xinghaixuanwo@outlook.com>
2019-09-11 14:05:24 +0800

    Change webpack performance optimization tips

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   // 禁止生成js sourceMap文件
   productionSourceMap: false,
+  // webpack 性能优化提示
+  configureWebpack: {
+    performance: {
+      hints: 'warning',
+      // 入口最大值
+      maxEntrypointSize: 1024000,
+      // 生成的资源文件最大值
+      maxAssetSize: 1024000,
+      // 只针对js文件给出性能优化提示
+      assetFilter: function (assetFilename) {
+        return assetFilename.endsWith(".js")
+      }
+    }
+  },
   pages: {
     popup: {
       entry: 'src/modules/popup/main.js',
@@ -14,11 +28,11 @@ module.exports = {
       filename: 'options.html',
       title: 'options'
     },
-    index: {
+    background: {
       entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'index'
+      template: 'public/background.html',
+      filename: 'background.html',
+      title: 'background'
     }
   }
 }
