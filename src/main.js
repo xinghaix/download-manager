49ad3a4b5f83465a8dd3025f7e021323c0389060
bobxli <bobxli@tencent.com>
2022-09-13 19:21:46 +0800

    (wip)fix: 增加通知防抖动

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import Background from './Background.vue'
+import { Executor } from './utils/lazyexecutor'
+
+oldcreate = chrome.notifications.create
+let exe = new Executor(null, 1000) //抖动区间1s
+chrome.notifications.create = (nid,op,callback) =>{
+  // TODO 加入合并通知的逻辑
+  exe.Execute(()=>{
+    oldcreate(nid,op,callback)
+  })
+}
 
 Vue.config.debug = false
 Vue.config.devtools = false
